c4e8b85b806c2ed8b2486bd581c59277d408d4d5
Mohammed Yusuf <mohammedyusufmujawar92@gmail.com>
2025-02-22 21:57:46 -0600

    - Completed the app. finished with the UI and now i am looking ways to save the question index and the score so when the user returns to the app they can continue where they left off.

--- app/src/main/java/com/example/quickquiz/MainActivity.kt
+++ app/src/main/java/com/example/quickquiz/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
